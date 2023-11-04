bcd96fa42f549f6736571de11d0cc9e2f1a0f005
Erick Macedo <32196382+erickmacedo@users.noreply.github.com>
2023-11-04 18:28:29 -0300

    Resolução do Exercicio 01

---
+++ Lógica de Programação/Exercicios/Lógica de Programação em C/Estrutura de Decisão/ex01.c
@@ -0,0 +1,38 @@
+/*
+1. Goku vs. Vegeta:
+
+Entre com dois valores distintos e exiba o mais poderoso.
+*/
+
+// Importação de Biblitoecas
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(int argc, char *argv[]) {
+
+    //Definicição do teclado para aceitar teclado brasileiro
+	
+	setlocale(LC_ALL, "portuguese_brazil");
+
+    //Declaração das variavéis
+
+    float goku, vedita;
+
+    //Entrada de Dados dos usuários
+
+    printf("Insira o valor do poder de Goku: ");
+    scanf("%f", &goku);
+    printf("Insira o valor do poder de Vedita: ");
+    scanf("%f", &vedita);
+
+    if(goku > vedita)
+        printf("Goku é o mais poderoso");
+    else if(vedita > goku)
+        printf("Vedita é mais poderoso");
+    else
+        printf("Os poderes são iguais!");
+
+	return 0;
+}
